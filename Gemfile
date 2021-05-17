32ded7dc7b06891200b0d0ab89ccdfe4654d4ac3
Septentrion50 <erwan56lecorre@gmail.com>
2021-05-17 11:07:24 +0200

    Ajout du paramétrage de variables d'environnement

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  # Open sent emails
+  gem 'letter_opener'
 end
 
 group :test do
@@ -68,3 +70,6 @@ gem 'jquery-rails'
 
 # Visualize table in console
 gem 'table_print'
+
+# Environement variables
+gem 'dotenv-rails'
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ApplicationMailer
+  default from: 'no-reply@monsite.fr'
+
+  def welcome_email(user)
+    # On recupere l'insance user pour la passer ensuite a la view en @user
+    @user = user
+
+    # Definition d'une variable @url qui sera utilisee ensuite dans la view d'e-mail
+    @url = 'http://monsite.fr/login'
+
+    # C'est l'appel de fonction mail qui envoie le mail a l'utilisateur
+    mail(to: @user.email, subject: 'Bienvenue shez nous !')
+  end
+end
